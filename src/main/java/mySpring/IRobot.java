4300c2b11446daa9d95011d2fec90ce5ba38eb35
Roman Garkavenko <roman.garkavenko@yandex.ua>
2015-12-13 15:49:21 +0300

    make it work

--- src/main/java/mySpring/IRobot.java
+++ src/main/java/mySpring/IRobot.java
@@ -11,7 +11,7 @@ public class IRobot {
     private Cleaner cleaner;
 
     public IRobot() {
-        System.out.println(speaker.getClass());
+//        System.out.println(speaker.getClass());    // NPE
     }
 
     public void cleanRoom(){
